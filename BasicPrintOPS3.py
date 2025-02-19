c8622b0573cfa1211471002692dbb9a6f3d9b977
ahsanshaikh27 <shaikhahsan8580@gmail.com>
2025-02-19 17:37:00 +0530

    add files

---
+++ BasicPrintOPS3.py
@@ -0,0 +1,4 @@
+print("Welcome to Python Programming")
+print(10 + 5) #Addition
+print("python" , "is", "Awesome")
+#Multiple Arguments
---
+++ Variable&DataTypes4.py
@@ -0,0 +1,4 @@
+name = "shaikh"
+age = 24
+is_learning = True
+print(name, age, is_learning)
