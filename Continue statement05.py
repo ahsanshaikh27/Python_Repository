4d527448b766245d7f7471383fa64cdf23a6079a
ahsanshaikh27 <shaikhahsan8580@gmail.com>
2025-04-15 17:28:45 +0530

    Initialize Commit

---
+++ Continue statement05.py
@@ -0,0 +1,6 @@
+# print("2 Continue statement (Skips the current iteration and continues with the next iteration")
+
+for i in range(5):
+   if i == 3:
+    continue # skips 3
+   print(i)
---
+++ Loop Control Statements04.py
@@ -0,0 +1,9 @@
+# print("break statemnent (Stops the loop")
+
+for i in range(10):
+    if i == 5:
+        break # stops at 5
+    print(i)
+
+
+
